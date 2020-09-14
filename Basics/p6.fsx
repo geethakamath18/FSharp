ddeefc2648be008d3525286709b99840576cbce4
Geetha Kamath Koteshwar <37272001+geethakamath18@users.noreply.github.com>
2020-09-14 14:54:59 -0400

    Tuples

---
+++ Basics/p6.fsx
@@ -0,0 +1,18 @@
+module Tuples =
+
+    /// A simple tuple of integers.
+    let tuple1 = (1, 2, 3)
+
+    /// A function that swaps the order of two values in a tuple. 
+    ///
+    /// F# Type Inference will automatically generalize the function to have a generic type,
+    /// meaning that it will work with any type.
+    let swapElems (a, b) = (b, a)
+
+    printfn "The result of swapping (1, 2) is %A" (swapElems (1,2))
+
+    /// A tuple consisting of an integer, a string,
+    /// and a double-precision floating point number.
+    let tuple2 = (1, "fred", 3.1415)
+
+    printfn "tuple1: %A\ttuple2: %A" tuple1 tuple2
